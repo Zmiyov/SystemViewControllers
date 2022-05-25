7f51eba8e59f8a1e10e4792239f98e0e237b9b79
Volodymyr Pysarenko <zmiyov88@gmail.com>
2022-05-25 13:42:01 +0300

    Initial Commit

---
+++ SystemViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SystemViewControllers
+//
+//  Created by Vladimir Pisarenko on 25.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
